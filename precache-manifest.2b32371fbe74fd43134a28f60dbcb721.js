e7c4cc695d29017d4423a989f888405a999567e5
Preeti Singh <impreetisingh14@gmail.com>
2025-05-30 13:27:50 -0400

    Updates

---
+++ precache-manifest.2b32371fbe74fd43134a28f60dbcb721.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56bae99b71d80db40f8174d12547c8f9",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "1c7d1b7534d3bb617602",
+    "url": "/my-portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9a0c0ab78b66898033eb",
+    "url": "/my-portfolio/static/js/2.5be66980.chunk.js"
+  },
+  {
+    "revision": "1eb98706859bae847c8762bbc3c10000",
+    "url": "/my-portfolio/static/js/2.5be66980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092690fe4a1b5e07a3e3",
+    "url": "/my-portfolio/static/js/3.a5d6bda9.chunk.js"
+  },
+  {
+    "revision": "1c7d1b7534d3bb617602",
+    "url": "/my-portfolio/static/js/main.6ee9bd4e.chunk.js"
+  },
+  {
+    "revision": "c095245982493c479bd2",
+    "url": "/my-portfolio/static/js/runtime-main.e031bf11.js"
+  }
+]);
