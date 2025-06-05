e9d58ec8a78832b8e7c6403df3ad4a10915d1e06
Preeti Singh <impreetisingh14@gmail.com>
2025-06-04 22:40:53 -0400

    updated project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
     <nav style={{ padding: '10px', background: '#eee' }}>
       <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
       <Link to="/about" style={{ marginRight: '10px' }}>About</Link>
-      <Link to="/Work" style={{ marginRight: '10px' }}>Projects</Link>
+      <Link to="/Projects" style={{ marginRight: '10px' }}>Projects</Link>
       <Link to="/contact">Contact</Link>
     </nav>
   );
